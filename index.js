975356222471e331cafec45c004e80313bbab67c
Erwin Everts <690259-erwineverts@users.noreply.gitlab.com>
2025-07-28 11:31:34 +0200

    Add type definitions and improve documentation for memoryPerWorker function

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const os = require('os');
 
+/**
+ * @param {number}  num - The number to clamp.
+ * @param {number} min - The minimum value.
+ * @param {number} max - The maximum value.
+ * @returns {number} The clamped value.
+ */
 function clamp(num, min, max) {
   return num <= min 
     ? min 
@@ -8,6 +14,11 @@ function clamp(num, min, max) {
       : num
 }
 
+/**
+ * Calculates the optimal number of workers based on system memory.
+ * @param {number} [memory=3] - The amount of memory (in GB) each worker should use.
+ * @returns {number} The optimal number of workers.
+ */
 function memoryPerWorker(memory = 3){
   const cpuCount = os.cpus().length;
   const systemMemory = Math.round(os.totalmem() / 1024 / 1024 / 1024);
